93ac39d1e341c8508506d2d778b95820cd48140f
LEENTechDev <leentechdev@gmail.com>
2021-07-28 14:01:49 +0800

    revert the try catch

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,7 +77,7 @@ class UserController extends Controller
             
             return $randomString;
         }
-        // try {
+        try {
             $user_details = User_profile::where('user_id', Auth::user()->user_id);
             if($user_details){
                 unset($request['user_name']);
@@ -103,8 +103,8 @@ class UserController extends Controller
             }else{
                 return response()->json(['message' => 'Account update failed!'], 409);
             }
-        // }catch (\Exception $e) {
-        //     return response()->json(['message' => 'Account update failed!'], 409);
-        // }
+        }catch (\Exception $e) {
+            return response()->json(['message' => 'Account update failed!'], 409);
+        }
     }
 }
