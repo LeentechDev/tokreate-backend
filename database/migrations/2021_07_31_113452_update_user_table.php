6ea900e5524f35b45881e03ed887f6f2d56d3c66
LEENTechDev <leentechdev@gmail.com>
2021-07-31 12:17:07 +0800

    Create 2021_07_31_113452_update_user_table.php
    add field for social media logins

---
+++ database/migrations/2021_07_31_113452_update_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->string('user_profile_facebook')->nullable();
+            $table->string('user_profile_twitter')->nullable();
+            $table->string('user_profile_instagram')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
