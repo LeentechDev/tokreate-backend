5a9441e185e93c0d468d1337dd00fa115b5884ac
LEENTechDev <leentechdev@gmail.com>
2021-08-26 13:48:27 +0800

    payment revision

---
+++ app/Edition.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+use App\TokenHistory;
+
+class Edition extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $primaryKey = 'edition_id';
+    protected $fillable = [
+        'token_id', 'owner_id', 'current_price', 'edition_no'
+    ];
+
+    protected $table = 'editions';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+    
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+    public function history(){
+        return $this->hasMany(TokenHistory::class, 'edition_id', 'edition_id');
+    }
+}
--- app/Transaction.php
+++ app/Transaction.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 use App\Token;
+use App\Edition;
 use App\User_profile;
 
 class Transaction extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
@@ -25,8 +26,10 @@ class Transaction extends Model implements AuthenticatableContract, Authorizable
     const UPDATED_AT = 'transaction_updated_at';
     protected $primaryKey = 'transaction_id';
     protected $fillable = [
-        'user_id','transaction_token_id', 'transaction_type','transaction_payment_method', 'transaction_details','transaction_service_fee', 'transaction_gas_fee', 'transaction_allowance_fee', 'transaction_token_price', 'transaction_grand_total', 'transaction_status', 'transaction_payment_status', 'transaction_urgency', 'transaction_payment_tnxid', 'transcation_computed_commission'
+        'user_id','transaction_token_id', 'transaction_type','transaction_payment_method', 'transaction_details','transaction_service_fee', 'transaction_gas_fee', 'transaction_allowance_fee', 'transaction_token_price', 'transaction_grand_total', 'transaction_status', 'transaction_payment_status', 'transaction_urgency', 'transaction_payment_tnxid', 'transaction_computed_commission'
     ];
+
+    protected $with = ['edition'];
 
     /**
      * The attributes excluded from the model's JSON form.
@@ -45,6 +48,10 @@ class Transaction extends Model implements AuthenticatableContract, Authorizable
 
     public function token(){
         return $this->belongsTo(Token::class, 'transaction_token_id', 'token_id'); 
+    }
+
+    public function edition(){
+        return $this->belongsTo(Edition::class, 'edition_id', 'edition_id'); 
     }
 
     public function collector(){
