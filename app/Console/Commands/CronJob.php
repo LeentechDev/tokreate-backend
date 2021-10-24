0a1b007532100d3305a696b30e728c0918f3679f
LEENTechDev <leentechdev@gmail.com>
2021-10-24 12:30:54 +0800

    Update CronJob.php

--- app/Console/Commands/CronJob.php
+++ app/Console/Commands/CronJob.php
@@ -75,20 +75,22 @@ class CronJob extends Command
                         foreach ($users as $user_id) {
                             $user = User::where('user_id', $user_id)->first();
 
-                            $email_msg = "<p>Dear <b>" . $user->profile->user_profile_full_name . "</b>, <br><br> We are continuing to develop more features on our websites. To address these changes, we've updated our <a href='" . ENV('APP_URL') . "/privacypolicy'>Privacy Policy</a>.</p>";
-                            $subject = "Privacy Policy";
+                            if($user){
+                                $email_msg = "<p>Dear <b>" . $user->profile->user_profile_full_name . "</b>, <br><br> We are continuing to develop more features on our websites. To address these changes, we've updated our <a href='" . ENV('APP_URL') . "/privacypolicy'>Privacy Policy</a>.</p>";
+                                $subject = "Privacy Policy";
 
-                            Mail::send('mail.email', ['msg' => $email_msg, 'title' => $subject], function ($message) use ($user, $subject) {
-                                $message->to($user->user_email, $user->profile->user_profile_full_name)->subject($subject);
-                                $message->from('support@tokreate.com', 'Tokreate');
-                            });
+                                Mail::send('mail.email', ['msg' => $email_msg, 'title' => $subject], function ($message) use ($user, $subject) {
+                                    $message->to($user->user_email, $user->profile->user_profile_full_name)->subject($subject);
+                                    $message->from('support@tokreate.com', 'Tokreate');
+                                });
 
-                            Notifications::create([
-                                'notification_message' => $email_msg,
-                                'notification_to' => $user_id,
-                                'notification_from' => 0,
-                                'notification_type' => Constants::NOTIF_MINTING_RES,
-                            ]);
+                                Notifications::create([
+                                    'notification_message' => $email_msg,
+                                    'notification_to' => $user_id,
+                                    'notification_from' => 0,
+                                    'notification_type' => Constants::NOTIF_MINTING_RES,
+                                ]);
+                            }
                         }
                         break;
 
@@ -97,20 +99,22 @@ class CronJob extends Command
                         foreach ($users as $user_id) {
                             $user = User::where('user_id', $user_id)->first();
 
-                            $email_msg = '<p>Hi <b>' . $user->profile->user_profile_full_name . '</b>, <br><br> </p>' . $cron->content;
-                            $subject = "Update on Commission Rate";
+                            if($user){
+                                $email_msg = '<p>Hi <b>' . $user->profile->user_profile_full_name . '</b>, <br><br> </p>' . $cron->content;
+                                $subject = "Update on Commission Rate";
 
-                            Mail::send('mail.transfer-status', ['msg' => $email_msg, 'title' => $subject], function ($message) use ($user, $subject) {
-                                $message->to($user->user_email, $user->profile->user_profile_full_name)->subject($subject);
-                                $message->from('support@tokreate.com', 'Tokreate');
-                            });
+                                Mail::send('mail.transfer-status', ['msg' => $email_msg, 'title' => $subject], function ($message) use ($user, $subject) {
+                                    $message->to($user->user_email, $user->profile->user_profile_full_name)->subject($subject);
+                                    $message->from('support@tokreate.com', 'Tokreate');
+                                });
 
-                            Notifications::create([
-                                'notification_message' => $email_msg,
-                                'notification_to' => $user_id,
-                                'notification_from' => 0,
-                                'notification_type' => Constants::NOTIF_MINTING_RES,
-                            ]);
+                                Notifications::create([
+                                    'notification_message' => $email_msg,
+                                    'notification_to' => $user_id,
+                                    'notification_from' => 0,
+                                    'notification_type' => Constants::NOTIF_MINTING_RES,
+                                ]);
+                            }
                         }
                         break;
 
